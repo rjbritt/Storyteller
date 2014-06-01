64e2f70dbe8723a9fe74284f0010d2e34c479522
Ryan Britt <rjbritt071@gmail.com>
2014-06-01 14:14:09 -0400

    Fixed Pods Installation

---
+++ Pods/Pods-Storyteller-RCDraggableButton-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Storyteller_RCDraggableButton : NSObject
+@end
+@implementation PodsDummy_Pods_Storyteller_RCDraggableButton
+@end
---
+++ Pods/Pods-StorytellerTests-RCDraggableButton-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StorytellerTests_RCDraggableButton : NSObject
+@end
+@implementation PodsDummy_Pods_StorytellerTests_RCDraggableButton
+@end
