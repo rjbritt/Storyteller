4f35737669f0ab262ca463c9700caf8cb91c54ac
Ryan Britt <rjbritt071@gmail.com>
2014-07-12 17:42:49 -0400

    added in missing dependencies

---
+++ Pods/Pods-StorytellerTests-MSSlidingPanelController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StorytellerTests_MSSlidingPanelController : NSObject
+@end
+@implementation PodsDummy_Pods_StorytellerTests_MSSlidingPanelController
+@end
